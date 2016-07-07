9696d8d566f99a6f20a7d09987f316db293f5034
root <root@python.(none)>
2016-07-07 16:29:00 +0800

    求素数

--- sushu.py
+++ sushu.py
@@ -1,7 +1,15 @@
-def sushu(num):                            
-    res=True
-    for x in range(2,num-1):
-        if num %x ==0:
-            res=False
-        return res
-print ([ x for x in range(100) if sushu(x)])
+def getPrime(maxNum):
+    aList = [x for x in range(0,maxNum)]
+    prime = []
+    for i in range(2,len(aList)):
+        if aList[i] != 0:
+            prime.append(aList[i])
+            clear(aList[i],aList,maxNum)
+    return prime
+    
+def clear(aPrime,aList,maxNum):
+    for i in range(2,int((maxNum/aPrime)+1)):
+        if not aPrime*i>maxNum-1:
+            aList[i*aPrime]=0
+
+print getPrime(100)
