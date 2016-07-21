2ea3e4669b4eb2e6358052e46f46aa12677894f6
root <root@saltstack1.(none)>
2016-07-21 15:04:44 +0800

    找出一个列表中，只出现了一次的数字， 并且保持原来的次序

--- keep_list_order.py
+++ keep_list_order.py
@@ -1,6 +1,16 @@
+'''Version1.1.0'''
 a= [1, 2, 1, 3, 2, 5]
 new_list=[]
 for i in a:
     if a.count(i) == 1:
        new_list.append(i)
+print (new_list)
+'''Version1.1.1'''
+a= [1, 2, 1, 3, 2, 5]
+new_list=[]
+for i in a:
+    if i not in new_list:
+       new_list.append(i)
+    else:
+       new_list.remove(i)
 print (new_list)
