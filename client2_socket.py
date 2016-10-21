8051200b3c718a4908ecbc5c484b9ebf54695b57
root <root@python.(none)>
2016-10-21 16:56:42 +0800

    完善客户端

--- client2_socket.py
+++ client2_socket.py
@@ -7,8 +7,8 @@ client.connect(ip_port)
 while True:
     msg = input(">>:").strip()
     if len(msg) == 0:continue
+    if msg == 'q':break
     client.send( msg.encode("utf-8") )
-
     data = client.recv(1024)
     print("来自服务器:",str(data,'utf8'))
 
